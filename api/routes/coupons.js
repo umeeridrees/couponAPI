230a526aa7debf2cbba1eb7f4d49c71ba6d2d26b
umeridrees <umer@cloudcade.pk>
2018-10-26 18:23:05 +0500

    added uuid and mongo

--- api/routes/coupons.js
+++ api/routes/coupons.js
@@ -1,24 +1,70 @@
 const express = require('express');
 const router = express.Router();
 
+const uuid = require('uuid/v4');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'myproject';
+const client = new MongoClient(url);
+
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    createCapped(db, function () {
+        client.close();
+    });
+});
+
+function createCapped(db, callback) {
+    db.createCollection("myCollection", {
+            "capped": true,
+            "size": 100000,
+            "max": 5000
+        },
+        function (err, results) {
+            console.log("Collection created.");
+            callback();
+        }
+    );
+
+    db.collection('myCollections').insertOne({
+        a: 1
+    }, function (err, r) {
+        assert.equal(null, err);
+        assert.equal(1, r.insertedCount);
+    });
+};
+
+
+
 // endpoint to generate coupons
 // api/coupons/generate/{number}
 router.post('/generate/:number', (req, res, next) => {
-    const number = req.params.number;
+    let number = req.params.number;
 
     //checking for invalid number of coupons
-    if(number < 1){
+    if (number < 1) {
         res.status(200).json({
             message: "Provide valid number."
         });
-    } else if(number >500){
+    } else if (number > 500) {
         res.status(200).json({
             message: "maximum 500 coupon can be generated."
         });
     } else {
-        res.status(200).json({
-            message: "generating coupons..."
-        });
+        let list = [];
+        while(number){
+            let uid = uuid(1000+number);
+            list.push(uid);
+            number--;
+        }
+        res.status(200).send(JSON.stringify(list));
     }
 });
 
@@ -26,13 +72,13 @@ router.post('/generate/:number', (req, res, next) => {
 // api/coupons/redeem/{code}
 router.post('/redeem/:code', (req, res, next) => {
     const code = req.params.code;
-    
+
     //Checking for invalid code
-    if(code < 1){
+    if (code < 1) {
         res.status(200).json({
             message: "invalid coupon."
         });
-    } else{
+    } else {
         res.status(200).json({
             message: "Redeeming your coupon..."
         });
